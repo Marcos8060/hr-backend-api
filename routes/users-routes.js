e347567efed2f8ed23a784d0519e97b2239cea3a
Marcos8060 <marcosgav80@gmail.com>
2024-11-07 02:49:06 +0300

    polished admin panel

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,17 +1,19 @@
 const route = require('express').Router();
 
 // import controllers
-const { RegisterUser, LoginUser,updateRole,fetchAllUsers } = require('../controllers/users-controller')
+const { RegisterUser, LoginUser,updateRole,fetchAllUsers,deleteUser,editUser } = require('../controllers/users-controller')
 const protectedRoute = require('../middleware/protected-routes')
 const authorizeRole = require('../middleware/role-based-access')
 
 
 
 // define routes
-route.post('/register', RegisterUser);
+route.post('/register',protectedRoute,authorizeRole('Admin'), RegisterUser);
 route.post('/login', LoginUser);
-route.get('/users', protectedRoute, authorizeRole('admin'), fetchAllUsers);
-route.put('/users/update-role', protectedRoute, authorizeRole('admin'), updateRole);
+route.get('/users', protectedRoute, fetchAllUsers);
+route.put('/users/update-role', protectedRoute, authorizeRole('Admin'), updateRole);
+route.delete('/delete-user/:id', protectedRoute, authorizeRole('Admin'), deleteUser);
+route.put('/edit-user/:id', protectedRoute, authorizeRole('Admin'), editUser);
 
 
 module.exports = route;

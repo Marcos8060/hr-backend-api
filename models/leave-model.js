3ec9e2f2dacd0b1ea1f4b8da9e30af6664fb8942
Marcos8060 <marcosgav80@gmail.com>
2024-11-07 18:08:12 +0300

    made changes

---
+++ models/leave-model.js
@@ -0,0 +1,78 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const dotenv = require("dotenv");
+const User = require("./authentication-model");
+
+dotenv.config();
+
+// initialize
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: "localhost",
+    dialect: "postgres",
+  }
+);
+
+// define models
+const Leave = sequelize.define(
+  "Leave",
+  {
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: User, // refers to User model
+        key: "id",
+      },
+    },
+    leaveType: {
+        type: DataTypes.ENUM("Annual Leave", "Medical Leave","Maternity Leave","Academic Leave"),
+      allowNull: false,
+     
+    },
+    fromDate: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      
+    },
+    toDate: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    days: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    reason: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Define association with user model
+User.hasOne(Leave, {
+  foreignKey: "userId",
+  as: "leave",
+  onDelete: "CASCADE",
+});
+Leave.belongsTo(User, { foreignKey: "userId" });
+
+// sync the model with the database
+sequelize
+  .sync()
+  .then(() => {
+    console.log(
+      "Leave model has been successfully defined and synchronized with the database."
+    );
+  })
+  .catch((error) => {
+    console.error("Unable to sync Leave model with the database:", error);
+  });
+
+module.exports = Leave;
